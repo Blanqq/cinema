aa2e00bf439068f2ea108496c46c1859940eab0c
Blanqq <vanderuard@gmail.com>
2019-03-26 00:05:43 +0000

    Welcome page authorization tests

---
+++ tests/Feature/WelcomePageTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class WelcomePageTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    public function testUnauthorizedUserNeedToLoginOrRegister()
+    {
+        $response = $this->get('/');
+        $response->assertStatus(200);
+        $response->assertSee('Please register or login to buy tickets');
+    }
+
+    public function testAuthorizedUserCanSeeLinkToRegistrationService()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+        $response = $this->get('/');
+        $response->assertStatus(200);
+        $response->assertSee('Go Reserve Tickets');
+    }
+}
