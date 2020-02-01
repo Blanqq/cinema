3f3febaf0c7dbbfe3574df7d3293601cdc86421f
Pawel <vanderuard@gmail.com>
2020-02-02 00:03:33 +0100

    New tests for Cinema CRUD features

--- tests/Feature/CinemaTest.php
+++ tests/Feature/CinemaTest.php
@@ -33,6 +33,96 @@ class CinemaTest extends TestCase
         $this->signInAs('User');
         $cinema = factory(Cinema::class)->make();
         $this->post('/cinemas/', $cinema->toArray())->assertStatus(403);
+    }
 
+    public function testAdminCanSeeListOfAllCinemas()
+    {
+        $this->signInAs('Admin');
+        $cinema = factory(Cinema::class)->create();
+        $this->get('/cinemas/')->assertSee($cinema->name);
+    }
+
+    public function testEmployeeCanSeeListOfAllCinemas()
+    {
+        $this->signInAs('Employee');
+        $cinema = factory(Cinema::class)->create();
+        $this->get('/cinemas/')->assertSee($cinema->name);
+    }
+
+    public function testUserCanNotSeeListOfAllCinemas()
+    {
+        $this->signInAs('User');
+        $cinema = factory(Cinema::class)->create();
+        $this->get('/cinemas/')->assertForbidden(); // 403
+    }
+
+    public function testAdminCanUpdateCinemaDetails()
+    {
+        $this->signInAs('Admin');
+        $cinema = factory(Cinema::class)->create(['street' => 'Random Street']);
+        $this->get('/cinemas')->assertSee($cinema->name);
+
+        $editedStreet = 'Edited Street';
+        $editedCity = 'Edited City';
+
+        $this->patch('/cinemas/'.$cinema->slug,['street' => $editedStreet, 'city' => $editedCity]);
+        $this->get('/cinemas/'.$cinema->fresh()->slug)
+            ->assertSee($editedStreet)
+            ->assertDontSee($cinema->street);
+    }
+
+    public function testEmployeeCanUpdateCinemaDetails()
+    {
+        $this->signInAs('Employee');
+        $cinema = factory(Cinema::class)->create(['street' => 'Random Street']);
+        $this->get('/cinemas')->assertSee($cinema->name);
+
+        $editedStreet = 'Edited Street';
+        $editedCity = 'Edited City';
+
+        $this->patch('/cinemas/'.$cinema->slug, ['street' => $editedStreet, 'city' => $editedCity]);
+        $this->get('/cinemas/'.$cinema->fresh()->slug)
+            ->assertSee($editedStreet)
+            ->assertDontSee($cinema->street);
+    }
+
+    public function testUserHaveNoAccessToPatchCinemaRoute()
+    {
+        $this->signInAs('User');
+        $cinema = factory(Cinema::class)->create();
+
+        $editedStreet = 'Edited Street';
+        $editedCity = 'Edited City';
+
+        $this->patch('/cinemas/'.$cinema->slug, ['street' => $editedStreet, 'city' => $editedCity])
+            ->assertForbidden();
+    }
+
+    public function testAdminCanDeleteCinema()
+    {
+        $this->signInAs('Admin');
+        $cinema = factory(Cinema::class)->create();
+        $this->assertDatabaseHas('cinemas', ['slug' => $cinema->slug]);
+
+        $this->delete('/cinemas/'.$cinema->slug);
+        $this->assertDatabaseMissing('cinemas', ['slug' => $cinema->slug]);
+    }
+
+    public function testEmployeeCanDeleteCinema()
+    {
+        $this->signInAs('Employee');
+        $cinema = factory(Cinema::class)->create();
+        $this->assertDatabaseHas('cinemas', ['slug' => $cinema->slug]);
+
+        $this->delete('/cinemas/'.$cinema->slug);
+        $this->assertDatabaseMissing('cinemas', ['slug' => $cinema->slug]);
+    }
+
+    public function testUserDoNotHaveAccessToDeleteCinemaRoute()
+    {
+        $this->signInAs('User');
+        $cinema = factory(Cinema::class)->create();
+
+        $this->delete('/cinemas/'.$cinema->slug)->assertForbidden();
     }
 }
