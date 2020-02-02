9ad6198c09e8a2c139a9cf5270e5bbe22dba90c8
Pawel <vanderuard@gmail.com>
2020-02-02 05:16:18 +0100

    Modifications in select day form. Added links to movies database in cinema show view

--- resources/views/cinema-page/show.blade.php
+++ resources/views/cinema-page/show.blade.php
@@ -2,19 +2,31 @@
 @section('content')
 
     <div class="col">
-        <div class="card">
+        <div class="card mb-2">
             <div class="card-header">
                 Select Day
             </div>
             <div class="card-body">
 
                         @foreach ($days_to_select as $day)
-                            <a href="/{{ $cinema->slug }}/?date={{ Carbon\Carbon::parse($day)->format('Y-m-d') }}">{{ Carbon\Carbon::parse($day)->format('d F Y') }}</a>
+                            <a href="/{{ $cinema->slug }}/?date={{ Carbon\Carbon::parse($day)->format('Y-m-d') }}">
+                                @if (Carbon\Carbon::parse($day)->format('Y-m-d') == Carbon\Carbon::parse($date)->format('Y-m-d'))
+                                <button class="btn btn-danger" disabled>
+                                    {{ Carbon\Carbon::parse($day)->format('d') }} <br>
+                                    {{ Carbon\Carbon::parse($day)->format('F') }}
+                                </button>
+                                @else
+                                <button class="btn btn-primary">
+                                    {{ Carbon\Carbon::parse($day)->format('d') }} <br>
+                                    {{ Carbon\Carbon::parse($day)->format('F') }}
+                                </button>
+                                @endif
+                            </a>
                         @endforeach
             </div>
         </div>
 
-        <div class="card">
+        <div class="card mb-2">
             <div class="card-header">
                 {{$cinema->name}} - Shows on day {{ $date->format('d F Y') }}
             </div>
@@ -24,7 +36,8 @@
                         @foreach ($shows_grouped_by_movie as $group_of_shows => $shows)
                         <tbody>
                             <tr>
-                                <td>{{ $group_of_shows }}</td>
+
+                            <td><a href="/movies/{{ $shows[0]->movie_id }}">{{ $group_of_shows }}</a></td>
                                 <td>|
                                     @foreach ($shows as $show)
                                         <a href="/cinemas/{{ $cinema->slug }}/shows/{{ $show->id }}">
