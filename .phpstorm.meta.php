c7163df1ad167538e0e9c8e3f0cc57f91049d47d
Pawel <vanderuard@gmail.com>
2020-02-06 01:47:11 +0100

    Dependencies update

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-12-08 22:36:37.
+    * Generated on 2020-02-05 23:46:20.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.8.35 on 2019-12-08 22:36:35.
+ * Generated for Laravel 5.8.36 on 2020-02-05 23:46:18.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
